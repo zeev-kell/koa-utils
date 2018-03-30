2d39080b1c82c23ae933f2a7a15cbe8b45813078
zeevkell <zeevkell@gmail.com>
2018-03-30 17:44:14 +0800

    Add error-body

---
+++ src/error-body.js
@@ -0,0 +1,25 @@
+module.exports = function ({serviceCode, IS_DEVELOP}) {
+  /**
+   * 处理异常
+   * @param error
+   * @param ctx
+   * @returns {{status: number, message: *|string, code: string, name, stack: boolean}}
+   */
+  return function (err, traceId) {
+    // 判断是否是接口返回的异常
+    if (err.response && err.response.data) {
+      err = err.response.data;
+    }
+    let body = {
+      status: (typeof err.status === 'number') ? err.status : 500,
+      message: err.message || err.toString(),
+      code: err.code || serviceCode,
+      name: err.name,
+      stack: IS_DEVELOP && (Array.isArray(err.stack) ? err.stack : err.stack.split('\n'))
+    }
+    if (traceId) {
+      body.id = traceId;
+    }
+    return body;
+  }
+}
