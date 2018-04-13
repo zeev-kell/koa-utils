bc5c453efe6328ee9c9ec694e5ab0e95b222e503
zeevkell <zeevkell@gmail.com>
2018-04-13 10:35:24 +0800

    add msg

--- src/error-body.js
+++ src/error-body.js
@@ -5,22 +5,29 @@ module.exports = function ({serviceCode, IS_DEVELOP}) {
    * @param ctx
    * @returns {{status: number, message: *|string, code: string, name, stack: boolean}}
    */
-  return function (err, traceId) {
+  return function (err, ctx, i18n = require('./i18n')) {
     // 判断是否是接口返回的异常
     if (err.response && err.response.data) {
       err = err.response.data;
     }
     /** @namespace body */
-    return {
+    let body = {
       status: (typeof err.status === 'number') ? err.status : 500,
       msg: err.msg,
       message: err.message || err.toString(),
       name: err.name,
-      formatting: err.formatting,
-      request_id: traceId,
+      request_id: ctx['X-B3-TraceId'] && ctx['X-B3-TraceId'].traceId,
       request_code: err.request_code || serviceCode,
-      server_time: new Date(),
-      stack: IS_DEVELOP ? (Array.isArray(err.stack) ? err.stack : err.stack.split('\n')) : undefined
+      server_time: new Date()
     }
+    // 没有翻译，默认调用 i18n 翻译
+    if (!body.msg) {
+      body.msg = i18n(ctx.get('Accept-Language'), err.message, err.formatting)
+    }
+    // 开发环境
+    if (IS_DEVELOP) {
+      body.stack = (Array.isArray(err.stack) ? err.stack : err.stack.split('\n'));
+    }
+    return body
   }
 }
--- src/prefix-rule.js
+++ src/prefix-rule.js
@@ -10,8 +10,8 @@ class PrefixRule {
    * @constructor
    */
   static async TimeValidation(ctx, next) {
-    errors.ifInvalid(ctx.query.startTime, null, 'startTime')
-    errors.ifInvalid(ctx.query.endTime, 'endTime')
+    errors.ifInvalid(ctx.query.startTime, 'invalid_key_parameter', 'startTime')
+    errors.ifInvalid(ctx.query.endTime, 'invalid_key_parameter', 'endTime')
     await next()
   }
 
@@ -25,8 +25,8 @@ class PrefixRule {
   static async PageValidation(ctx, next) {
     let currentPage = parseInt(ctx.query.currentPage)
     let pageSize = parseInt(ctx.query.pageSize)
-    errors.ifInvalid(currentPage > 0, null, 'currentPage')
-    errors.ifInvalid(pageSize > 0, null, 'pageSize')
+    errors.ifInvalid(currentPage > 0, 'invalid_key_parameter', 'currentPage')
+    errors.ifInvalid(pageSize > 0, 'invalid_key_parameter', 'pageSize')
     ctx.query.currentPage = currentPage
     ctx.query.pageSize = pageSize
     await next()
